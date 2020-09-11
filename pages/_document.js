c95f6f9dfb34a4e5f19a15aa2f4a63f926f0243d
Bryan Mutai <mutaiwork@gmail.com>
2020-09-11 14:48:04 +0300

    move to lib/gtag and event handling/recording

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -12,19 +13,25 @@ class MyDocument extends Document {
                 <Head>
                     <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
+                    {/* Global Site Tag (gtag.js) - Google Analytics */}
+                    <script
+                        async
+                        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                    />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', '${GA_TRACKING_ID}', {
+                                page_path: window.location.pathname,
+                            });
+                            `,
+                        }}
+                    />
                 </Head>
                 <body className="antialiased" style={{ backgroundColor: '#A7E0F1', fontFamily: 'PT Sans Narrow' }}>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177620241-1"></script>
-                    <script type='text/javascript'>
-                        {`
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){
-                            dataLayer.push(arguments);
-                        }
-                        gtag('js', new Date());
-                        gtag('config', 'UA-177620241-1');
-                    `}
-                    </script>
                     <Main />
                     <NextScript />
                 </body>
