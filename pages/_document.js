8065a84ab43395df3e648d1d6b64b022cefc0880
Bryan Mutai <mutaiwork@gmail.com>
2020-09-11 14:24:32 +0300

    Add analytics boilerplate

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,17 @@ class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"></link>
                 </Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-177620241-1"></script>
+                <script type='text/javascript'>
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){
+                            dataLayer.push(arguments);
+                        }
+                        gtag('js', new Date());
+                        gtag('config', 'UA-177620241-1');
+                    `}
+                </script>
                 <body className="antialiased" style={{ backgroundColor: '#A7E0F1', fontFamily: 'PT Sans Narrow' }}>
                     <Main />
                     <NextScript />
