c79ed85b12c2f7894aca699ba37276e7f4de066c
Bryan Mutai <mutaiwork@gmail.com>
2020-09-17 13:04:59 +0300

    Fix cookie header

--- next.config.js
+++ next.config.js
@@ -4,5 +4,38 @@ module.exports = withPWA({
     pwa: {
         dest: 'public',
         disable: true
-    }
+    },
+    async headers() {
+        return [
+            {
+                source: '/contact',
+                headers: [
+                    {
+                        key: 'Set-Cookie',
+                        value: 'SameSite=None;Secure',
+                    },
+                ],
+            },
+        ]
+    },
 })
+
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/about',
+                headers: [
+                    {
+                        key: 'x-custom-header',
+                        value: 'my custom header value',
+                    },
+                    {
+                        key: 'x-another-custom-header',
+                        value: 'my other custom header value',
+                    },
+                ],
+            },
+        ]
+    },
+}
