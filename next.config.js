9730c3d8f6a05d6d96e21a80e235098252177f08
Bryan Mutai <mutaiwork@gmail.com>
2020-09-17 13:28:39 +0300

    Fix header

--- next.config.js
+++ next.config.js
@@ -19,23 +19,3 @@ module.exports = withPWA({
         ]
     },
 })
-
-module.exports = {
-    async headers() {
-        return [
-            {
-                source: '/about',
-                headers: [
-                    {
-                        key: 'x-custom-header',
-                        value: 'my custom header value',
-                    },
-                    {
-                        key: 'x-another-custom-header',
-                        value: 'my other custom header value',
-                    },
-                ],
-            },
-        ]
-    },
-}
