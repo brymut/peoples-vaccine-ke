aa7cad99d3a7c70bf55a992eb4ef8a2d13181014
Bryan Mutai <mutaiwork@gmail.com>
2020-09-18 10:29:06 +0300

    Fix meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -15,11 +15,12 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <meta charSet="utf-8" />
                     <meta name='application-name' content={APP_NAME} />
+                    <meta name="Description" content={APP_DESCRIPTION} />
                     <meta name='apple-mobile-web-app-capable' content='yes' />
                     <meta name='apple-mobile-web-app-status-bar-style' content='default' />
                     <meta name='apple-mobile-web-app-title' content={APP_NAME} />
-                    <meta name="description" content={APP_DESCRIPTION} />
                     <meta name='format-detection' content='telephone=no' />
                     <meta name='mobile-web-app-capable' content='yes' />
                     <meta name="robots" content="index, follow" />
@@ -31,9 +32,9 @@ class MyDocument extends Document {
                     <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
                     <link rel='manifest' href='/manifest.json' />
                     <link rel='shortcut icon' href='/icons/favicon.ico' />
-                    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
+                    <link defer href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+                    <link defer href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+                    <link defer href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;900&display=swap" rel="stylesheet" />
                     {/* Global Site Tag (gtag.js) - Google Analytics */}
                     <script
                         async
