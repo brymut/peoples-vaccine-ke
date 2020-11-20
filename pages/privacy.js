c6a67ef0ca0661f0c1e8aef93bbd26f80dcbbe7e
Bryan Mutai <mutaiwork@gmail.com>
2020-11-20 11:22:45 +0300

    updated publications with link and fixed privacy page

--- pages/privacy.js
+++ pages/privacy.js
@@ -1,4 +1,5 @@
 import Navigation from '../components/Navigation'
+import Layout from  '../components/Layout'
 import Head from 'next/head'
 import DirectusSDK from "@directus/sdk-js";
 
@@ -25,27 +26,38 @@ export async function getStaticProps() {
 
 
 export default function PrivacyPolicyPage({ privacyPolicyHtml, optOut, setOptOut, dismissPrivacyBanner, setDismissPrivacyBanner }) {
+    const seo = {
+        title: "#PeoplesVaccineKE - Privacy",
+        description: "The #PeoplesVaccineKE privacy policy statement.",
+        canonical: "https://peoplesvaccine.co.ke/privacy",
+        openGraph: {
+            url: 'https://peoplesvaccine.co.ke/privacy',
+            title: '#PeoplesVaccineKE - Privacy',
+            description: 'The #PeoplesVaccineKE privacy policy statement.',
+            images: [
+                {
+                    url: 'https://peoplesvaccine.co.ke/images/logo-banner.jpg',
+                    width: 800,
+                    height: 292,
+                    alt: 'Og Image Alt',
+                },
+                {
+                    url: 'https://peoplesvaccine.co.ke/images/logo-banner.png',
+                    width: 1244,
+                    height: 454,
+                    alt: 'Og Image Alt Second',
+                }
+            ],
+        }
+    }
     return (
-        <>
-            <Head>
-                <title>#PeoplesVaccineKE - Privacy</title>
-                <meta name="title" content="#PeoplesVaccineKE - Privacy" />
-                <meta name="description" content="The #PeoplesVaccineKE privacy policy statement." />
-                <meta name="twitter:title" content="#PeoplesVaccineKE - Privacy" />
-                <meta name="twitter:description" content="The #PeoplesVaccineKE privacy policy statement." />
-                <meta property="og:title" content="#PeoplesVaccineKE - Privacy" />
-                <meta property="og:description" content="The #PeoplesVaccineKE privacy policy statement." />
-                <meta name="twitter:url" content="https://peoplesvaccine.co.ke/privacy" />
-                <meta property="og:url" content="https://peoplesvaccine.co.ke/privacy" />
-                <meta property="og:type" content="website" />
-            </Head>
-            <Navigation optOut={optOut} setOptOut={setOptOut} dismissPrivacyBanner={dismissPrivacyBanner} setDismissPrivacyBanner={setDismissPrivacyBanner} />
+        <Layout seo={seo} optOut={optOut} setOptOut={setOptOut} dismissPrivacyBanner={dismissPrivacyBanner} setDismissPrivacyBanner={setDismissPrivacyBanner}>
             <div className='mt-12 lg:mt-4 lg:mx-32 mx-8 mb-12 '>
                 {optOut ? <p>You have opted out of Google Analytics cookie tracking</p> : <p>Google Analytics tracking via cookies is currently enabled</p>}
                 <p>This privacy policy takes effect on the 21st of September 2020</p>
                 <p>There are currently no revisions, this is the first version.</p>
             </div>
             <div className="mt-8 lg:mt-4 lg:mx-32 mx-8 mb-32 lg:items-center privacy-policy" dangerouslySetInnerHTML={{ __html: privacyPolicyHtml }}></div>
-        </>
+        </Layout>
     )
 }
